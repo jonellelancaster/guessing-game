e3603ebf901526e4edb01b0247c32ccd2a48155d
jonellelancaster <jonellelancaster@gmail.com>
2019-08-24 12:55:34 -0400

    second attempt

--- src/main/java/guessinggame/GuessingGame.java
+++ src/main/java/guessinggame/GuessingGame.java
@@ -5,43 +5,45 @@ import java.util.Scanner;
 
 public class GuessingGame {
 
+	static Scanner input = new Scanner(System.in);
+	static Random rand = new Random();
 	public static void main(String[] args) {
 		System.out.println("Welcome to the guessing game");
 		System.out.println("Make a guess:");
-		Scanner input = new Scanner(System.in);
-//		Random rand = new Random();
-		
-//		int winningNumber = rand.nextInt(11);
-		int winningNumber=7;
+
+		int winningNumber = rand.nextInt(11);
+
 		int guess = input.nextInt();
 		int guessCount = 0;
-		
-		System.out.println("You guessed " + guess);
 
-		while (guess != -1 && guessCount < 2) {
-			if (guess == 0) {
+		while (guess != -1 && guessCount < 3) {
+			System.out.println("You guessed " + guess);
+				
+			if(guess == 0||guess>=11) {
 				System.out.println("Try to guess a number. Please pick a number 1-10");
-			} else if (guess != winningNumber) {
-				
-				System.out.println("Wrong Answer");
-				if (guessCount== winningNumber) {
-					System.out.println("You get one more guess");
-				}
-				if (guess < winningNumber) {
-					System.out.println();
-				}
-					guessCount++;
+			}else  if ( guess < winningNumber) {
+				System.out.println(" Wrong Answer, Too Low!");
+			} else if  (guess > winningNumber) {
+				System.out.println("Wrong answer, Too High!");
+			} else {
+				System.out.println("You Win!");
 			}
-		if (guess == winningNumber) {
-			System.out.println("You Win!");
-			break;
-		}
-				
+
+			guessCount++;
+			if (guessCount < 3 && guess != winningNumber) {
+				System.out.println("You get another guess");
+			}
 			
+			if (guessCount < 3 || guess == 0||guess>=11) {
+
+				guess = input.nextInt();
+			}
 		}
-		guess = input.nextInt();
-		System.out.println("guess again");
+		
+		System.out.println("Thanks for playing");
 
 	}
+	
+		
+	}
 
-}
